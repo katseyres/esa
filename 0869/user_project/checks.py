1cbaff5130d20358c8d42263df11904e39d14588
Maximilien <maximilien.denis@five.be>
2022-01-09 11:33:56 +0100

    Add user_project algorithm

---
+++ 0869/user_project/checks.py
@@ -0,0 +1,76 @@
+import string
+import re
+
+
+def checkPostCode(postCode:int) -> bool:
+    """Check if the post code contains exaclty 4 digits."""
+    return (type(postCode) == int and len(str(postCode)) == 4)
+
+def checkEmail(email:str) -> bool:
+    """The email must match with the pattern xxx@xxx.xx and must only contain alpha lowercase, integers or "." and "-"."""
+    pattern = r"^[a-z0-9\.\-]{3}@[a-z0-9\.\-]{3}\.[a-z0-9\.\-]{2}$"
+    output = re.match(pattern, email)
+
+    return output is not None
+
+def checkPassword(password:str) -> bool:
+    """The password must contain at least
+        - 10 characters.
+        - One lowercase alpha.
+        - One uppercase alpha.
+        - One digit.
+        - One special character.  
+    """
+    alphaLowerPattern = r"[a-z]"
+    alphaUpperPattern = r"[A-Z]"
+    digitPattern = r"[0-9]"
+    specialCharPattern = r"[^a-z^A-Z^0-9]"
+
+    hasAlphaLower = re.search(alphaLowerPattern, password)
+    hasAlphaUpper = re.search(alphaUpperPattern, password)
+    hasDigit =  re.search(digitPattern, password)
+    hasSpecialChar =  re.search(specialCharPattern, password)
+
+    # print(hasAlphaLower)
+    # print(hasAlphaUpper)
+    # print(hasDigit)
+    # print(hasSpecialChar)
+    
+    if (len(password) < 10):
+        return False
+    elif (hasAlphaUpper is None):
+        return False
+    elif (hasAlphaUpper is None):
+        return False
+    elif (hasDigit is None):
+        return False
+    elif (hasSpecialChar is None):
+        return False
+
+    return True
+
+
+def checkUsername(username:str) -> bool:
+    """The username must contain only lowercase alpha or digits."""
+    pattern = r"^[a-z0-9]+$"
+    return re.match(pattern, username) is not None
+
+
+def checkName(firstName:str, lastName:str) -> bool:
+    """Firstname and lastname must contain only letters or "-"."""
+
+    chars = string.ascii_letters + "-"
+
+    firstNameChecked = True
+    lastNameChecked = True
+        
+    for letter in firstName:
+        if chars.__contains__(letter) is False:
+            firstNameChecked = False
+            break
+
+    for letter in lastName:
+        if chars.__contains__(letter) is False:
+            lastNameChecked = False
+
+    return firstNameChecked and lastNameChecked
--- 0869/user_project/main.py
+++ 0869/user_project/main.py
@@ -1,81 +1,96 @@
-import string
-import re
-from sys import hash_info
+from checks import checkPostCode, checkName, checkPassword, checkUsername, checkEmail
 
-def checkName(firstName:str, lastName:str) -> bool:
-    """CHeck if parameters only to contain letters or "-"."""
+"""
+katseyres
+Admin123456_
+Maximilien
+Denis
+5170
+den@den.fr
 
-    chars = string.ascii_letters + "-"
+pabloooo
+PaBloooo123__
+Pablo
+Escobar
+3298
+pab@pab.pa
+"""
 
-    firstNameChecked = True
-    lastNameChecked = True
-        
-    for letter in firstName:
-        if chars.__contains__(letter) is False:
-            firstNameChecked = False
-            break
+username = ""
+password = ""
+firstname = ""
+lastname = ""
+postCode = 0
+email = ""
 
-    for letter in lastName:
-        if chars.__contains__(letter) is False:
-            lastNameChecked = False
+dataRightEncoded = False
 
-    return firstNameChecked and lastNameChecked
+username = input("Select an username: ")
+password = input("Select a password: ")
+firstname = input("Select a firstname: ")
+lastname = input("Select a lastname: ")
+postCode = int(input("Select a post code: "))
+email = input("Select an email: ")
 
-def checkPostCode(postCode:int) -> bool:
-    """Check if the post code contains exaclty 4 digits."""
-    return (type(postCode) == int and len(str(postCode)) == 4)
+def displayUserInfo(password, firstname, lastname, email, postCode, username):
+    print(
+        f"\n\
+    USER INFORMATION\n\
+    | Username: {username}\n\
+    | Password: {'*' * len(password)}\n\
+    | Firstname: {firstname}\n\
+    | Lastname: {lastname}\n\
+    | Post code: {postCode}\n\
+    | Email: {email}"
+    )
 
-def checkEmail(email:str) -> bool:
-    """Check if the email matches with the pattern xxx@xxx.xx."""
-    pattern = r"^[a-z0-9\.\-]{3}@[a-z0-9\.\-]{3}\.[a-z0-9\.\-]{2}$"
-    output = re.match(pattern, email)
-    print('Email match: {0}'.format(output))
-
-    return output is not None
-
-def checkUsername(username:str) -> bool:
-    """Check if the username only contains lowercase alpha or digits."""
-    pattern = r"^[a-z0-9]+$"
-    return re.match(pattern, username) is not None
-
-def checkPassword(password:str) -> bool:
-    """Check if the password contains at least
-        - One lowercase alpha.
-        - One uppercase alpha.
-        - One digit.
-        - One special character.  
-    """
-    alphaLowerPattern = r"[a-z]"
-    alphaUpperPattern = r"[A-Z]"
-    digitPattern = r"[0-9]"
-    specialCharPattern = r"[^a-z^A-Z^0-9]"
-
-    hasAlphaLower = re.search(alphaLowerPattern, password)
-    hasAlphaUpper = re.search(alphaUpperPattern, password)
-    hasDigit =  re.search(digitPattern, password)
-    hasSpecialChar =  re.search(specialCharPattern, password)
-
-    print(hasAlphaLower)
-    print(hasAlphaUpper)
-    print(hasDigit)
-    print(hasSpecialChar)
+def allTests(password, firstname, lastname, email, postCode, username):
+    if checkPassword(password) is False:
+        print(f"\n/!\\ {checkPassword.__doc__}")
+        password = input(f"--> Please select another password: ")
+        print(password)
+        return False
+    if checkName(firstname, lastname) is False:
+        print(f"\n/!\\ {checkName.__doc__}")
+        firstname = input("--> Please select another firstname: ")
+        lastname = input("--> Please select another lastname: ")
+        return False
+    if checkEmail(email) is False:
+        print(f"\n/!\\ {checkEmail.__doc__}")
+        email = input("--> Please select another email: ")
+        return False
+    if checkPostCode(postCode) is False:
+        print(f"\n/!\\ {checkPostCode.__doc__}")
+        postCode = input("--> Please select another post code: ")
+        return False
+    if checkUsername(username) is False:
+        print(f"\n/!\\ {checkUsername.__doc__}")
+        username = input("--> Please select another username: ")
+        return False
     
-    if (len(password) < 10):
-        return False
-    elif (hasAlphaUpper is None):
-        return False
-    elif (hasAlphaUpper is None):
-        return False
-    elif (hasDigit is None):
-        return False
-    elif (hasSpecialChar is None):
-        return False
-
     return True
 
+while dataRightEncoded is False:
+    dataRightEncoded = allTests(password, firstname, lastname, email, postCode, username)
+    
+displayUserInfo(password, firstname, lastname, email, postCode, username)
 
-print(checkName("Maximilien", "Denis"))
-print(checkPostCode(2333))
-print(checkEmail("eee@zzz.zz"))
-print(checkUsername("eeduzfiu1"))
-print(checkPassword("ozeEi8cb_rfy"))
+response = input("\nDo you want add a second user? (y/n) ").lower()
+
+if response == "y":
+    secondUsername = input("Select an username: ")
+    password = input("Select a password: ")
+    firstname = input("Select a firstname: ")
+    lastname = input("Select a lastname: ")
+    postCode = int(input("Select a post code: "))
+    email = input("Select an email: ")
+
+    while secondUsername == username:
+        secondUsername = input("/!\ Usersname already taken, please select another one: ")
+
+    dataRightEncoded = False
+   
+    while dataRightEncoded is False:
+        dataRightEncoded = allTests(password, firstname, lastname, email, postCode, username)
+        
+    displayUserInfo(password, firstname, lastname, email, postCode, secondUsername)
